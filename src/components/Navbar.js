16e0fab8889f54bdca2be877923ebc18c5767ed2
Vinicios Pereira da Silva <97298193+vinicios-psilva@users.noreply.github.com>
2024-02-08 16:31:20 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,24 +9,24 @@ const Navbar = () => {
         <a href="/" className="navbar-brand">
             <img src="favicon.ico" alt="logo" className="logo"/>
         </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse justify-content-end " id="navbarNav">
-            <ul className="navbar-nav mr-auto ">
-                <li className="nav-item ">
+            <ul className="navbar-nav mr-auto d-flex justify-content-center ">
+                <li className="nav-item">
                     <Link href="/" legacyBehavior>
-                        <a className="nav-link text-white">HOME</a>
+                        <a className="nav-link text-center">HOME</a>
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link href="/blog" legacyBehavior>
-                        <a className="nav-link text-white">BLOG</a>
+                        <a className="nav-link text-center">BLOG</a>
                     </Link>
                 </li>
                 <li className="nav-item">
                     <Link href="/sobre" legacyBehavior>
-                        <a className="nav-link text-white">SOBRE MIM</a>
+                        <a className="nav-link text-center">SOBRE MIM</a>
                     </Link>
                 </li>
             </ul>
